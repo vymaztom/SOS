d3682725092e025533c8796c5750f3a7c817b831
vyma.96@gmail.com <>
2021-04-23 23:13:22 +0200

    update

--- install_graphic.sh
+++ install_graphic.sh
@@ -1,6 +1,8 @@
 dnf -y install xorg-x11-server-Xorg xinit
 dnf -y install wget zip
-
+unzip otter.zip -d /
+rm -f otter.zip
+dnf -y remove wget zip
 
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf -y install qt5-qtwebkit
