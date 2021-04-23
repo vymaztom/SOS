c2c13a37b206d3f15d7d1c38e03d595d51229d10
vyma.96@gmail.com <>
2021-04-23 18:57:08 +0200

    update

--- install_graphic.sh
+++ install_graphic.sh
@@ -1,6 +1,7 @@
 dnf -y install xorg-x11-server-Xorg xinit
 dnf -y install wget zip
 
+
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf -y install qt5-qtwebkit
 touch ~/.xinitrc
