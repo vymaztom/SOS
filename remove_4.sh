5eca44cabdd32d2cda7ebb11dbe8b02b07e18e29
vyma.96@gmail.com <>
2021-04-23 22:22:29 +0200

    update

--- remove_4.sh
+++ remove_4.sh
@@ -3,3 +3,15 @@ do
 	echo "  removing $file"
 	sudo yum -y remove $file
 done
+
+for file in $(rpm -qa | grep ssh)
+do
+	echo "  removing $file"
+	sudo yum -y remove $file
+done
+
+for file in $(rpm -qa | grep vim)
+do
+	echo "  removing $file"
+	sudo yum -y remove $file
+done
