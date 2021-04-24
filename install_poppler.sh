bfe99cb980270b985c565af650abfa99134c1572
vyma.96@gmail.com <>
2021-04-24 11:04:12 +0200

    update

--- install_poppler.sh
+++ install_poppler.sh
@@ -1,1 +1,2 @@
-rpm -y install poppler-utils-0.26.5-43.el7.1.x86_64.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/poppler-utils-0.26.5-43.el7.x86_64.rpm
+rpm poppler-utils-0.26.5-43.el7.1.x86_64.rpm
