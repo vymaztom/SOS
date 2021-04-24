8c97907dedbaac62a761f0180cdedd3f80c9e4f7
vyma.96@gmail.com <>
2021-04-24 14:00:53 +0200

    update

--- remove_4.sh
+++ remove_4.sh
@@ -1,16 +1,4 @@
-for file in $(rpm -qa | grep python)
-do
-	echo "  removing $file"
-	sudo yum -y remove $file
-done
-
-for file in $(rpm -qa | grep ssh)
-do
-	echo "  removing $file"
-	sudo yum -y remove $file
-done
-
-for file in $(rpm -qa | grep vim)
+for file in $(rpm -qa | grep $1)
 do
 	echo "  removing $file"
 	sudo yum -y remove $file
